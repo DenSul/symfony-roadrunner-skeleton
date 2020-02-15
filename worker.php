600560085ef8a1f7944ddcf37b4209ed9ecdde7f
Sultanov Denis <sultanden@gmail.com>
2020-02-15 18:50:15 +0500

    [feature/github-action] added flow and CS/PHPStan for dev env. (#4)
    * [feature/github-action] added flow and run tests.

--- worker.php
+++ worker.php
@@ -8,8 +8,8 @@ use Spiral\RoadRunner\PSR7Client;
 use Spiral\RoadRunner\Worker;
 use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 use Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory;
-use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\Dotenv\Dotenv;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 use Zend\Diactoros\ResponseFactory;
 use Zend\Diactoros\ServerRequestFactory;
@@ -19,16 +19,14 @@ use Zend\Diactoros\UploadedFileFactory;
 require 'config/bootstrap.php';
 
 // The check is to ensure we don't use .env in production
-if (! isset($_SERVER['APP_ENV']) && ! isset($_ENV['APP_ENV'])) {
-    if (! class_exists(Dotenv::class)) {
-        throw new \RuntimeException(
-            'APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.'
-        );
+if (!isset($_SERVER['APP_ENV']) && !isset($_ENV['APP_ENV'])) {
+    if (!class_exists(Dotenv::class)) {
+        throw new \RuntimeException('APP_ENV environment variable is not defined. You need to define environment variables for configuration or add "symfony/dotenv" as a Composer dependency to load variables from a .env file.');
     }
-    (new Dotenv)->load(__DIR__ . '/.env');
+    (new Dotenv())->load(__DIR__.'/.env');
 }
 
-$env   = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
+$env = $_SERVER['APP_ENV'] ?? $_ENV['APP_ENV'] ?? 'dev';
 $debug = (bool) ($_SERVER['APP_DEBUG'] ?? $_ENV['APP_DEBUG'] ?? ('prod' !== $env));
 
 if ($debug) {
@@ -46,20 +44,20 @@ if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false
     Request::setTrustedHosts(explode(',', $trustedHosts));
 }
 
-$kernel                = new Kernel($env, $debug);
-$relay                 = new StreamRelay(STDIN, STDOUT);
-$psr7                  = new PSR7Client(new Worker($relay));
-$httpFoundationFactory = new HttpFoundationFactory;
-$psrHttpFactory        = new PsrHttpFactory(
-    new ServerRequestFactory,
-    new StreamFactory,
-    new UploadedFileFactory,
-    new ResponseFactory
+$kernel = new Kernel($env, $debug);
+$relay = new StreamRelay(STDIN, STDOUT);
+$psr7 = new PSR7Client(new Worker($relay));
+$httpFoundationFactory = new HttpFoundationFactory();
+$psrHttpFactory = new PsrHttpFactory(
+    new ServerRequestFactory(),
+    new StreamFactory(),
+    new UploadedFileFactory(),
+    new ResponseFactory()
 );
 
 while ($req = $psr7->acceptRequest()) {
     try {
-        $request  = $httpFoundationFactory->createRequest($req);
+        $request = $httpFoundationFactory->createRequest($req);
         $response = $kernel->handle($request);
         $psr7->respond($psrHttpFactory->createResponse($response));
         $kernel->terminate($request, $response);
